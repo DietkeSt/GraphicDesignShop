3567c081c7c50a7b02afb13fd090abb79cc439a0
dietkest <dietkesteindel@gmail.com>
2024-02-18 18:45:12 +0000

    Adjust path for product detail and category list view

--- store/views.py
+++ store/views.py
@@ -17,9 +17,9 @@ def all_products(request):
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', {'category': category, 'products': products})
+    return render(request, 'products/category.html', {'category': category, 'products': products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'products/detail.html', {'product': product})
