112ee4fe2e2d2d9cc4fabe0595d64ab3df5b9a3b
dietkest <dietkesteindel@gmail.com>
2024-02-18 12:22:35 +0000

    Add logout URL path and static settings url

--- artistic_edge/urls.py
+++ artistic_edge/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,7 +24,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
-]
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
