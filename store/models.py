d63a165a7ba3f95034b59f692ebcb55980ea7ca0
dietkest <dietkesteindel@gmail.com>
2024-02-19 19:36:44 +0000

    Add ProductMnager model to filter products and call it in the Product model

--- store/models.py
+++ store/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+    
+
 class Category(models.Model):
     name = models.CharField(max_length=50, db_index=True)
     slug = models.SlugField(max_length=50, unique=True)
@@ -31,6 +36,8 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = 'Products'
