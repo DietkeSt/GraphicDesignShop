e8d9c10afd566abf086273709f4e1349fd2dd228
dietkest <dietkesteindel@gmail.com>
2024-02-19 20:46:07 +0000

    Add store basket url path

--- artistic_edge/urls.py
+++ artistic_edge/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
+    path('basket/', include('store_basket.urls', namespace='store_basket')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
