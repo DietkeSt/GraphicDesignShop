17d6bc9393752462573f864d0bbfff644d2d0b70
dietkest <dietkesteindel@gmail.com>
2024-03-18 18:03:24 +0000

    Add update and delete views for basket

--- store_basket/views.py
+++ store_basket/views.py
@@ -6,7 +6,8 @@ from store.models import Product
 
 
 def basket_summary(request):
-    return render(request, 'basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'basket/summary.html', {'basket': basket})
 
 def add_to_basket(request):
     basket = Basket(request)
@@ -19,4 +20,29 @@ def add_to_basket(request):
         basketqty = basket.__len__()
         response = JsonResponse({'qty':basketqty})
 
+        return response
+    
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
