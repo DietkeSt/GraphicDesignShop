c8bfe0228845c3d0edd7a53100adaac8e782078e
dietkest <dietkesteindel@gmail.com>
2024-03-18 16:31:47 +0000

    Add product quantity to click event for add button

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,7 @@ $(document).on('click', '#add-button', function (e) {
         url: '{% url "basket:add_to_basket" %}',
         data: {
             product_id: $('#add-button').val(),
+            productqty: $('#select option: selected').text(),
             csrfmiddlewaretoken: "{{csrf_token}}",
             action: 'post'
         },
