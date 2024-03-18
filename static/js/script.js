94ed343ba71fd6b914fa0a7c3c97d7fd01f5650c
dietkest <dietkesteindel@gmail.com>
2024-03-18 16:09:17 +0000

    Add click event for add button for basket

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,17 @@
-$(document).ready(function(){
-    $('.add-to-basket').click(function(){
-        var productId = $(this).data('value');
-        var quantity = $(this).data('quantity');
+$(document).on('click', '#add-button', function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'POST',
+        url: '{% url "basket:add_to_basket" %}',
+        data: {
+            product_id: $('#add-button').val(),
+            csrfmiddlewaretoken: "{{csrf_token}}",
+            action: 'post'
+        },
+        success: function (json) {
 
-        $.ajax({
-            type: 'POST',
-            url: '{% url "store_basket:add_to_basket" %}',
-            data: {
-                'csrfmiddlewaretoken': '{{ csrf_token }}',
-                'product_id': productId,
-                'quantity': quantity,
-            },
-            success: function(response) {
-                // Update basket quantity display
-                $('#basket-qty').text(response.basket_total);
-            },
-            error: function(xhr, errmsg, err) {
-                console.log("Error adding item to basket: " + errmsg);
-            }
-        });
+        },
+        error: function (xhr, errmsg, err) {}
     });
-});
+})
+    
