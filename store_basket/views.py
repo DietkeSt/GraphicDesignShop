176afb855cc499ff9b683d9c8392ecee9c7a0d4a
dietkest <dietkesteindel@gmail.com>
2024-03-18 16:15:24 +0000

    Create basket summary and add to basket view

--- store_basket/views.py
+++ store_basket/views.py
@@ -1,24 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST
 
 from .models import Basket
 from store.models import Product
 
 
 def basket_summary(request):
-    basket = Basket(request)
-    return render(request, 'basket/summary.html', {'basket': basket})
+    return render(request, 'store/basket/summary.html')
 
-
-@require_POST
 def add_to_basket(request):
-    product_id = int(request.POST.get('product_id'))
-    quantity = request.POST.get('quantity', 1)
     basket = Basket(request)
-
-    product = get_object_or_404(Product, id=product_id)
-    basket.add(product=product, qty=quantity)
-
-    response = JsonResponse({'basket_total': len(basket)})
-    return response
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product)
