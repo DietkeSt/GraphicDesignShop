7dec1fdba7c59dfa1923b71d73ae21d1788bacda
dietkest <dietkesteindel@gmail.com>
2024-03-18 18:04:25 +0000

    Add delete and update path to URLs

--- store_basket/urls.py
+++ store_basket/urls.py
@@ -12,5 +12,7 @@ app_name = 'store_basket'
 urlpatterns = [
     path('', views.basket_summary, name='basket_summary'),
     path('add/', views.add_to_basket, name='add_to_basket'),
+    path('delete/', views.basket_delete, name='basket_delete'),
+    path('update/', views.basket_update, name='basket_update'),
 ]
     
