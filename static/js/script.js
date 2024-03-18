cf39f7ecebc54243a7a16046bd544c7d7500d99e
dietkest <dietkesteindel@gmail.com>
2024-03-18 18:18:24 +0000

    Add functions to delete and update basket items

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+// Add Item to basket
 $(document).on('click', '#add-button', function (e) {
     e.preventDefault();
     var url = $(this).data('url');
@@ -17,4 +18,48 @@ $(document).on('click', '#add-button', function (e) {
         error: function (xhr, errmsg, err) {}
     });
 })
+
+// Delete Item from basket
+$(document).on('click', '.delete-button', function (e) {
+    e.preventDefault();
+    var prodid = $(this).data('index');
+    var url = $(this).data('delete-url');
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: {
+        productid: $(this).data('index'),
+        csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
+        action: 'post'
+      },
+      success: function (json) {
+        $('.product-item[data-index="' + prodid + '"]').remove();
+        document.getElementById("subtotal").innerHTML = json.subtotal;
+        document.getElementById("basket-qty").innerHTML = json.qty
+      },
+      error: function (xhr, errmsg, err) {}
+    });
+  })
+
+  // Update Item in basket
+  $(document).on('click', '.update-button', function (e) {
+    e.preventDefault();
+    var prodid = $(this).data('index');
+    var url = $(this).data('update-url');
+    $.ajax({
+      type: 'POST',
+      url: url,
+      data: {
+        productid: $(this).data('index'),
+        productqty: $('#select' + prodid + ' option:selected').val(),
+        csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val(),
+        action: 'post'
+      },
+      success: function (json) {
+        document.getElementById("basket-qty").innerHTML = json.qty
+        document.getElementById("subtotal").innerHTML = json.subtotal
+      },
+      error: function (xhr, errmsg, err) {}
+    });
+  })
     
