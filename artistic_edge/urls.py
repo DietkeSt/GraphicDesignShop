0ad89bde7dbe46712b679005e53661f09d2d3219
dietkest <dietkesteindel@gmail.com>
2024-03-18 21:30:56 +0000

    Remove login and logout URL paths

--- artistic_edge/urls.py
+++ artistic_edge/urls.py
@@ -14,8 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
     path('basket/', include('store_basket.urls', namespace='store_basket')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('account/', include('account.urls', namespace='account')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
