6fa370e7a03e7647052735efe4dfc6862980a23b
dietkest <dietkesteindel@gmail.com>
2024-03-23 21:26:34 +0000

    Add UserBaseAdmin model

--- account/admin.py
+++ account/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import UserBase
 
+
+class UserBaseAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'email', 'profile_image_thumbnail')  # Add profile image to list display
+
+    def profile_image_thumbnail(self, obj):
+        if obj.profile_image:
+            return '<img src="%s" width="50" height="50" />' % obj.profile_image.url
+        else:
+            return 'No image'
+    profile_image_thumbnail.allow_tags = True
+    
+
 admin.site.register(UserBase)
