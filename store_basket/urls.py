67c0113745e9c55327f6e75b939675ad2f20717c
dietkest <dietkesteindel@gmail.com>
2024-03-23 17:17:49 +0000

    Add clear basket URL path

--- store_basket/urls.py
+++ store_basket/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('add/', views.add_to_basket, name='add_to_basket'),
     path('delete/', views.basket_delete, name='basket_delete'),
     path('update/', views.basket_update, name='basket_update'),
+    path('clear/', views.clear_basket, name='clear_basket'),
 ]
     
