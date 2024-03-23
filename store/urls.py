f8c7c3e939efc99151becabc78de6f6edbc9b819
dietkest <dietkesteindel@gmail.com>
2024-03-23 17:25:38 +0000

    Add test URL path to store to see if URL are loading as expected

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ URL configuration for store app.
 """
 
 from django.urls import path
+from django.http import HttpResponse
 
 from . import views
 
@@ -11,6 +12,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
+    path('test/', lambda request: HttpResponse("Test page works!"), name='test'),
     path('', views.all_products, name='all_products'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
