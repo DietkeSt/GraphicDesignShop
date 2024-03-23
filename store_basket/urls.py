b95c25f382e2d6be81ac85bec65c7a29ca022cfc
dietkest <dietkesteindel@gmail.com>
2024-03-23 17:27:03 +0000

    Add test URL to store basket to check if URL is loading as expected

--- store_basket/urls.py
+++ store_basket/urls.py
@@ -4,12 +4,13 @@ URL configuration for store_basket app.
 """
 
 from django.urls import path
-
+from django.http import HttpResponse
 from . import views
 
 app_name = 'store_basket'
 
 urlpatterns = [
+    path('testbasket/', lambda request: HttpResponse("Basket test page works!"), name='test_basket'),
     path('', views.basket_summary, name='basket_summary'),
     path('add/', views.add_to_basket, name='add_to_basket'),
     path('delete/', views.basket_delete, name='basket_delete'),
