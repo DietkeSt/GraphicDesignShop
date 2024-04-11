803cffb1206d0d468d40a90bb95d44c2aa82fe6c
dietkest <dietkesteindel@gmail.com>
2024-04-11 12:39:41 +0100

    Adjust path to clearly define just adding an item to wishlist

--- account/urls.py
+++ account/urls.py
@@ -49,6 +49,6 @@ urlpatterns = [
     path("addresses/set_default/<slug:id>/", views.set_default, name="set_default"),
     # Wish List
     path("wishlist/", views.wishlist, name="wishlist"),
-    path("wishlist/add_to_wishlist/<int:id>/", views.add_to_wishlist, name="user_wishlist"),
+    path("wishlist/add/<int:id>/", views.add_to_wishlist, name="add_to_wishlist"),
     path('wishlist/remove/<int:id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
