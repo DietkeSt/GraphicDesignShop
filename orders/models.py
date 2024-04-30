4e77a0e6655866d85b833dbab6ecd158d6fe124f
dietkest <dietkesteindel@gmail.com>
2024-04-30 16:56:33 +0100

    Add email for order status sending to orders model

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,9 @@
-from decimal import Decimal
-from django.conf import settings
 from django.db import models
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.contrib.sites.models import Site
+from django.conf import settings
 
 from store.models import Product
 
@@ -26,6 +29,38 @@ class Order(models.Model):
     )
     order_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='received')
     digital_product = models.FileField(upload_to='orders/products/', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Check if the order is new or the status has changed
+        if not self.pk:
+            is_new = True
+            old_status = None
+        else:
+            is_new = False
+            old_status = Order.objects.get(pk=self.pk).order_status
+
+        super().save(*args, **kwargs)
+
+        if is_new or (old_status != self.order_status):
+            self.send_status_email()
+
+    def send_status_email(self):
+        domain = Site.objects.get_current().domain
+        subject = f"Update on Your Order #{self.id}"
+        template_name = f'orders/emails/order_{self.order_status}_email.html'
+        
+        message_html = render_to_string(template_name, {
+            'order': self,
+            'domain': domain
+        })
+        message_plain = strip_tags(message_html)
+        send_mail(
+            subject,
+            message_plain,
+            'artisticedge.noreply@gmail.com',
+            [self.user.email],
+            html_message=message_html,
+        )
 
     class Meta:
         ordering = ('-created',)
