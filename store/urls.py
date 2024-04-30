444b61f2d3364f48437ad67c4f812ab1dce625c1
dietkest <dietkesteindel@gmail.com>
2024-04-30 18:39:12 +0100

    Add contact URL

--- store/urls.py
+++ store/urls.py
@@ -13,6 +13,7 @@ app_name = 'store'
 
 urlpatterns = [
     path('test/', lambda request: HttpResponse("Test page works!"), name='test'),
+    path('contact/', views.contact_form_submit, name='contact_form_submit'),
     path('', views.all_products, name='all_products'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
