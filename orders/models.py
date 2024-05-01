eba77541a3f7d86572c45015654e8020ed45c8d9
dietkest <dietkesteindel@gmail.com>
2024-05-01 20:07:20 +0100

    Update review model to use custom user

--- orders/models.py
+++ orders/models.py
@@ -86,10 +86,11 @@ class OrderItem(models.Model):
 
 class Review(models.Model):
     product = models.ForeignKey('store.Product', on_delete=models.CASCADE, related_name='reviews')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     rating = models.IntegerField(default=1, choices=[(i, str(i)) for i in range(1, 6)])
     comment = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.rating} stars by {self.user.username} for {self.product.title}"
+
