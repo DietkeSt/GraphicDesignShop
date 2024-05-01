658608d8647d913ade7cfc83688be841fab7eddc
dietkest <dietkesteindel@gmail.com>
2024-05-01 19:37:46 +0100

    Add submit-review URL path

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 from . import views
+from orders.views import submit_review
 
 
 app_name = 'store'
@@ -14,6 +15,7 @@ app_name = 'store'
 urlpatterns = [
     path('test/', lambda request: HttpResponse("Test page works!"), name='test'),
     path('contact/', views.contact_form_submit, name='contact_form_submit'),
+    path('submit-review/<int:product_id>/', submit_review, name='submit-review'),
     path('', views.all_products, name='all_products'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
