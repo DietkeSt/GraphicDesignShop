64db3f1b85375ffb69319248f572953ed3420fd5
dietkest <dietkesteindel@gmail.com>
2024-05-05 00:04:27 +0100

    Update stripe webhook and basket view to handle payment correctly

--- payment/views.py
+++ payment/views.py
@@ -28,23 +28,17 @@ class Error(TemplateView):
 
 @login_required
 def BasketView(request):
-    logger.debug(f"Using Stripe API key: {settings.STRIPE_SECRET_KEY}")
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-
     basket = Basket(request)
     total = str(basket.get_total_price())
     total = total.replace('.', '')
     total = int(total)
 
-    order_key = request.session.get('order_key')
-
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     intent = stripe.PaymentIntent.create(
         amount=total,
         currency='eur',
-        metadata={'order_key': order_key}
+        metadata={'userid': request.user.id}
     )
-
-    print("Stripe Intent created with order_key:", order_key)
 
     return render(request, 'payment/process_payment.html', {'client_secret': intent.client_secret, 
                                                             'STRIPE_PUBLISHABLE_KEY': os.environ.get('STRIPE_PUBLISHABLE_KEY')})
@@ -60,14 +54,14 @@ def stripe_webhook(request):
             json.loads(payload), stripe.api_key
         )
     except ValueError as e:
-        print("Error parsing webhook payload:", e)
+        print(e)
         return HttpResponse(status=400)
 
+    # Handle the event
     if event.type == 'payment_intent.succeeded':
-        order_key = event.data.object.metadata['order_key']
-        print("Webhook received for order_key:", order_key)  # Check the console for this output
-        payment_confirmation(order_key)
-        return HttpResponse(status=200)
+        payment_confirmation(event.data.object.client_secret)
 
-    print("Unhandled event type:", event.type)
+    else:
+        print('Unhandled event type {}'.format(event.type))
+
     return HttpResponse(status=200)
