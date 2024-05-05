51d4b61ea90c1d4466131d47339025553ba49c46
dietkest <dietkesteindel@gmail.com>
2024-05-05 21:35:01 +0100

    Overhaul add view for orders to include the correct field values

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponseNotAllowed
 from django.db.models import Avg
 
 from store_basket.models import Basket
@@ -13,34 +13,44 @@ from store.models import Product
 
 
 def add(request):
+    if request.method != 'POST':
+        # Restricting to POST requests
+        return HttpResponseNotAllowed(['POST'])
+
     basket = Basket(request)
-    if request.POST.get('action') == 'post':
+    order_key = request.POST.get('order_key')
+    user_id = request.user.id
+    baskettotal = basket.get_total_price()
+    buyer_note = request.POST.get('buyer_note', '')
 
-        order_key = request.POST.get('order_key')
-        user_id = request.user.id
-        baskettotal = basket.get_total_price()
-        buyer_note = request.POST.get('note', '') 
+    # Check if order exists
+    if Order.objects.filter(order_key=order_key).exists():
+        return JsonResponse({'error': 'Order already exists'}, status=400)
 
-        # Check if order exists
-        if Order.objects.filter(order_key=order_key).exists():
-            pass
-        else:
-            order = Order.objects.create(
-                user_id=user_id,
-                full_name='name',
-                address1='add1',
-                address2='add2', 
-                total_paid=baskettotal,
-                order_key=order_key,
-                buyer_note=buyer_note
-                )
-            order_id = order.pk
+    # Creating a new order
+    order = Order.objects.create(
+        user_id=user_id,
+        full_name=request.POST.get('full_name', 'No name provided'),
+        phone=request.POST.get('phone', 'No phone provided'),
+        address_line=request.POST.get('address_line', 'No address provided'),
+        address_line2=request.POST.get('address_line2', 'No address provided'),
+        town_city=request.POST.get('town_city', 'No city provided'),
+        country=request.POST.get('country', ''),
+        postcode=request.POST.get('postcode', 'No postcode provided'),
+        total_paid=baskettotal,
+        order_key=order_key,
+        buyer_note=buyer_note
+    )
 
-            for item in basket:
-                OrderItem.objects.create(order_id=order_id, product=item['product'], price=item['price'], quantity=item['qty'])
+    for item in basket:
+        OrderItem.objects.create(
+            order=order,
+            product=item['product'],
+            price=item['price'],
+            quantity=item['qty']
+        )
 
-        response = JsonResponse({'success': 'Return something'})
-        return response
+    return JsonResponse({'success': 'Order successfully placed!', 'order_id': order.id})
 
 
 def payment_confirmation(data):
