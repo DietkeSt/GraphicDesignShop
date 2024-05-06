56f16e8e5032cf78a7935566e50bd84b18a2e2ea
dietkest <dietkesteindel@gmail.com>
2024-05-06 21:20:15 +0100

    Add 404 and 500 handler to urls

--- artistic_edge/urls.py
+++ artistic_edge/urls.py
@@ -9,6 +9,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import handler404, handler500
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,7 +22,8 @@ urlpatterns = [
     path('', include('store.urls', namespace='store')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+
+handler404 = 'artistic_edge.views.handler404'
+handler500 = 'artistic_edge.views.handler500'
